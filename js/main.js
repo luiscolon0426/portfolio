5bd5342fae50457e5a51362025d7f0bf160f58ae
luiscolon0426 <lusantiago@protonmail.com>
2026-07-20 20:24:52 -0400

    updated Work

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   form();
   skillbar();
-  initProjectsCarousel();
   initSkillsCarousels();
 
   const nav = document.querySelector("#nav");
